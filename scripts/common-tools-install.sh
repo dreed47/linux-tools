bce65b0baaafc3dfd2fa1d2d4b01e5b92f41e845
David Reed <212698+dreed47@users.noreply.github.com>
2024-06-06 12:25:22 -0400

    updates

--- scripts/common-tools-install.sh
+++ scripts/common-tools-install.sh
@@ -3,13 +3,4 @@
 apt-get update
 apt-get install fzf
 
-grep -qxF 'fh() {' ~/.bashrc || cat << 'EOF' >> ~/.bashrc
-
-fh() {
-  local cmd=\$(history | fzf | sed "s/^[ ]*[0-9]*[ ]*//")
-  if [ -n "\$cmd" ]; then
-    eval "\$cmd"
-  fi
-}
-
-EOF
+awk 'BEGIN { found=0 } /^fh\(\) \{$/ { found=1; exit } END { if (!found) { system("cat << EOF >> ~/.bashrc\n\nfh() {\n  local cmd=$(history | fzf | sed \"s/^[ ]*[0-9]*[ ]*//\")\n  if [ -n \"$cmd\" ]; then\n    eval \"$cmd\"\n  fi\n}\n\nEOF") } }' ~/.bashrc

962369b9805691d358d112737bc84ede9fa7d6c0
David Reed <212698+dreed47@users.noreply.github.com>
2024-06-06 14:19:08 -0400

    updatres

--- scripts/common-tools-install.sh
+++ scripts/common-tools-install.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
+echo "Updating package lists..."
 apt-get update
+echo "Installing fzf..."
 apt-get install fzf
 
-# Download .bashrc_custom from the remote location
+echo "Downloading .bashrc_custom from the remote location..."
 curl -sSfL https://github.com/dreed47/linux-tools/raw/main/scripts/.bashrc_custom -o ~/.bashrc_custom
 
 # Check if the bashrc_custom call already exists in ~/.bashrc
 if ! grep -qxF "source ~/.bashrc_custom" ~/.bashrc; then
+  echo "Adding source ~/.bashrc_custom to ~/.bashrc..."
   echo "source ~/.bashrc_custom" >> ~/.bashrc
+else
+  echo "~/.bashrc_custom is already sourced in ~/.bashrc"
 fi
 
+echo "Reloading ~/.bashrc..."
 source ~/.bashrc
+
+echo " "
+echo "Script execution completed."

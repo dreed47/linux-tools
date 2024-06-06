bf2f6394567d94dab337e68f160ba1000b4d7dc8
David Reed <212698+dreed47@users.noreply.github.com>
2024-06-06 13:16:19 -0400

    updates

--- scripts/common-tools-install.sh
+++ scripts/common-tools-install.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install fzf
 
-# Define the function in a variable
+# Define the function and aliases in variables
 fh_function='fh() {
   local cmd=$(history | fzf | sed "s/^[ ]*[0-9]*[ ]*//")
   if [ -n "$cmd" ]; then
@@ -12,4 +12,17 @@ fh_function='fh() {
 }'
 
 # Check if the function already exists in ~/.bashrc
-grep -qxF "$fh_function" ~/.bashrc || echo "$fh_function" >> ~/.bashrc
+if ! grep -qxF "fh() {" ~/.bashrc; then
+  echo "$fh_function" >> ~/.bashrc
+fi
+
+# Define the aliases
+aliases='
+alias fcd="cd \$(find * -type d | fzf)"
+alias vf="vim \$(fzf)"
+alias fhf="history | fzf"'
+
+# Check if the aliases already exist in ~/.bashrc
+if ! grep -qxF "alias fcd=" ~/.bashrc || ! grep -qxF "alias vf=" ~/.bashrc || ! grep -qxF "alias fhf=" ~/.bashrc; then
+  echo "$aliases" >> ~/.bashrc
+fi

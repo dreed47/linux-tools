0fa3864ca93c8f8abd781245ecbeddf09940cda2
David Reed <212698+dreed47@users.noreply.github.com>
2024-06-06 12:55:52 -0400

    update

--- scripts/common-tools-install.sh
+++ scripts/common-tools-install.sh
@@ -3,13 +3,13 @@
 apt-get update
 apt-get install fzf
 
-grep -qF 'fh() {' ~/.bashrc || cat << 'EOF' >> ~/.bashrc
-
-fh() {
-  local cmd=\$(history | fzf | sed "s/^[ ]*[0-9]*[ ]*//")
-  if [ -n "\$cmd" ]; then
-    eval "\$cmd"
+# Define the function in a variable
+fh_function='fh() {
+  local cmd=$(history | fzf | sed "s/^[ ]*[0-9]*[ ]*//")
+  if [ -n "$cmd" ]; then
+    eval "$cmd"
   fi
-}
+}'
 
-EOF
+# Check if the function already exists in ~/.bashrc
+grep -qxF "$fh_function" ~/.bashrc || echo "$fh_function" >> ~/.bashrc

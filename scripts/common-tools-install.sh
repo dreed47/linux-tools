6cbbdf25d69d451d7f431c26d16ebad65a123740
David Reed <212698+dreed47@users.noreply.github.com>
2024-06-06 14:21:54 -0400

    updates

--- scripts/common-tools-install.sh
+++ scripts/common-tools-install.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
+# Define colors for status messages
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
-echo "Updating package lists..."
+echo "${YELLOW}Updating package lists...${NC}"
 apt-get update
-echo "Installing fzf..."
+echo "${YELLOW}Installing fzf...${NC}"
 apt-get install fzf
 
-echo "Downloading .bashrc_custom from the remote location..."
+echo "${YELLOW}Downloading .bashrc_custom from the remote location...${NC}"
 curl -sSfL https://github.com/dreed47/linux-tools/raw/main/scripts/.bashrc_custom -o ~/.bashrc_custom
 
 # Check if the bashrc_custom call already exists in ~/.bashrc
 if ! grep -qxF "source ~/.bashrc_custom" ~/.bashrc; then
-  echo "Adding source ~/.bashrc_custom to ~/.bashrc..."
+  echo "${YELLOW}Adding source ~/.bashrc_custom to ~/.bashrc...${NC}"
   echo "source ~/.bashrc_custom" >> ~/.bashrc
 else
-  echo "~/.bashrc_custom is already sourced in ~/.bashrc"
+  echo "${YELLOW}~/.bashrc_custom is already sourced in ~/.bashrc${NC}"
 fi
 
-echo "Reloading ~/.bashrc..."
+echo "${YELLOW}Reloading ~/.bashrc...${NC}"
 source ~/.bashrc
 
 echo " "
-echo "Script execution completed."
+echo "${YELLOW}Script execution completed.${NC}"

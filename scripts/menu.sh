5a6ef2b656f9317bc058c0510e114739234f2f9a
David Reed <212698+dreed47@users.noreply.github.com>
2024-06-09 14:42:51 -0400

    updaes

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -8,14 +8,18 @@ declare -A options
 options=(
     ["Reload bashrc"]="source ~/.bashrc; source_bashrc_with_message"
     ["LXC Updater"]='bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/misc/update-lxcs.sh)"'
+    ["List all LXC containers"]="pct list"
     ["List All Virtual Machines"]="qm list"
+    ["Show Proxmox Version"]="pveversion"
     ["Update custom bashrc"]="source ~/.bashrc; custom_bashrc_update"
 )
 
 # Array of option labels, including the Quit option
 option_labels=("Reload bashrc"
                "LXC Updater"
+               "List all LXC containers"
                "List All Virtual Machines"
+               "Show Proxmox Version"
                "Update custom bashrc"
                "Quit")
 
